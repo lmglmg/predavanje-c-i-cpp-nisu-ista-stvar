fe89fdeae7b342b6ef6fecac880e9a456e599daa
Luka Mate Granić <luka.granic@microblink.com>
2019-11-26 21:31:40 +0100

    Adding examples written in C

---
+++ examples/part_00_sleep/main.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <chrono>
+#include <thread>
+
+int main()
+{
+    using namespace std::chrono;
+
+    auto const start = high_resolution_clock::now();
+    std::this_thread::sleep_for( milliseconds( 100 ) );
+    auto const end = high_resolution_clock::now();
+
+    auto const seconds = duration_cast< duration< float, seconds::period > >( end-start ).count();
+
+    printf( "%f\n", seconds );
+}
---
+++ examples/part_00_sleep/main_fixed.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include <time.h>
+#include <stdio.h>
+
+int main()
+{
+    struct timespec t1;
+    clock_gettime( CLOCK_MONOTONIC, &t1 );
+
+    usleep( 100 * 1000 );
+
+    struct timespec t2;
+    clock_gettime( CLOCK_MONOTONIC, &t2 );
+
+    float seconds = ( float )( t2.tv_sec - t1.tv_sec ) + ( float )( t2.tv_nsec - t1.tv_nsec ) / 1e9f;
+
+    printf( "%f\n", seconds );
+}
