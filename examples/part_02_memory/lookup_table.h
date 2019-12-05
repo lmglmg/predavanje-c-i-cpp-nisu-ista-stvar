47a7cd5c2ecec908fc54aef4353dd3adcaff3554
Luka Mate Granić <luka.granic@microblink.com>
2019-12-05 11:03:18 +0100

    Small improvements

--- examples/part_02_memory/lookup_table.h
+++ examples/part_02_memory/lookup_table.h
@@ -20,7 +20,7 @@ float f( int const x, int const y, float const rot )
     float const a = atan2f( yT, xT );
     float const f = 0.23f;
     float const k = 3.0f;
-    return ( sin( f + k * r - M_PI / 2 ) / ( r * r * k ) + sin( f + k * r ) / r ) * sin( a + rot );
+    return ( cos( f + k * r - M_PI / 2 ) / ( r * r * k ) + cos( f + k * r ) / r ) * sin( a + rot );
 }
 
 int clamp( int value, int minValue, int maxValue )
@@ -32,10 +32,10 @@ int clamp( int value, int minValue, int maxValue )
 
 void print_value( float const value )
 {
-    char const * characters = " -~=+*owz%#@";
+    char const characters[] = " -~=+*oxqwz%#@";
 
     float const highLimit = 2.f;
-    int const numCharacters = 12;
+    int const numCharacters = sizeof(characters) - 1;
 
     float const absValue = fabsf( value );
     int index = clamp( absValue / highLimit * numCharacters, 0, numCharacters - 1 );
